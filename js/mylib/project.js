b9d2e4aaf7eca5acc452fdcf9fd7e1cd73357ef9
Aya Nakazawa <aya@ayatec.top>
2017-05-22 13:25:34 +0900

    Add js/mylib's
    Add js/mylib/common.js
    Add js/mylib/confirm.js
    Add js/mylib/init.js
    Add js/mylib/popover.js
    Add js/mylib/process-stack.js
    Add js/mylib/project.js
    Add js/mylib/scroll.js
    Add js/mylib/static-class.js
    Add js/mylib/switch.js
    Add js/mylib/util-class.js

---
+++ js/mylib/project.js
@@ -0,0 +1,17 @@
+
+// ----------------------------------------------------------------
+// Project
+
+class Project {
+  static get NAME() {
+    return 'Business Card Bank';
+  }
+
+  static get NAME_ABBREVIATION() {
+    return 'BCB';
+  }
+
+  static get NAME_KEY() {
+    return 'BCB';
+  }
+}
