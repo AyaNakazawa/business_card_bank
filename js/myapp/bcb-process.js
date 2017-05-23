a00c333ccc94a637d47c624f1743a7cf6d219f55
Aya Nakazawa <aya@ayatec.top>
2017-05-23 09:20:51 +0900

    Add initPopover in BCBProcess

--- js/myapp/bcb-process.js
+++ js/myapp/bcb-process.js
@@ -5,5 +5,30 @@ class BCBProcess extends CommonProcess {
       name: `${Project.NAME} Process`
     });
     
+    this.initProcess();
+  }
+  
+  initProcess() {
+    this.initPopover();
+  }
+  
+  initPopover() {
+    {
+      new PopoverController({
+        name: 'ID Popover',
+        selector: '#login-id-help',
+        help: 'IDを入力してください'
+      });
+      new PopoverController({
+        name: 'Password Popover',
+        selector: '#login-password-help',
+        help: 'パスワードを入力してください'
+      });
+      new PopoverController({
+        name: 'Login Check Popover',
+        selector: '#login-check-help',
+        help: '共有デバイスでは設定に注意してください。'
+      });
+    }
   }
 }
