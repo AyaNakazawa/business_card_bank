c9dc6f4ad64d62ada649357aff4dbcddd989f8e1
Aya Nakazawa <aya@ayatec.top>
2017-05-24 16:42:48 +0900

    Add some .class in Content

--- js/mylib/content.js
+++ js/mylib/content.js
@@ -3,5 +3,27 @@
 // Content Class
 
 class Content {
+  static get CONTENT() {
+    return 'content';
+  }
   
+  static get ITEM() {
+    return 'content-item';
+  }
+  
+  static get ITEM_HEADER() {
+    return 'content-item-header';
+  }
+  
+  static get ITEM_NAME() {
+    return 'content-item-name';
+  }
+  
+  static get ITEM_KEYS() {
+    return 'content-item-keys';
+  }
+  
+  static get ITEM_KEY() {
+    return 'content-item-key';
+  }
 }
