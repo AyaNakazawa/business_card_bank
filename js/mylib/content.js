5e77a35b8934e56f8320895af7b9066592557f61
Aya Nakazawa <aya@ayatec.top>
2017-05-24 17:19:52 +0900

    Add function's in Content
    getHeader
    getItemName
    getItemKey

--- js/mylib/content.js
+++ js/mylib/content.js
@@ -15,8 +15,8 @@ class Content {
     return 'content-item';
   }
   
-  static get ITEM_HEADER() {
-    return 'content-item-header';
+  static get HEADER() {
+    return 'content-header';
   }
   
   static get ITEM_NAME() {
@@ -31,14 +31,49 @@ class Content {
     return 'content-item-key';
   }
   
-  static createContentItem({
+  static getHeader(_header = null) {
+    return '';
+  }
+  
+  static getItemName(_name = null) {
+    return '';
+  }
+  
+  static getItemKey(..._keys) {
+    let result = '';
+    return result;
+  }
+  
+  static getItem({
     type = Content.TYPE_NAME_KEY,
+    contentId = null,
     header = null,
     name = null,
     keys = null
   } = {}) {
     let result = '';
     
+    if (contentId != null) {
+      result += `<div class="${Content.CONTENT}" id="${contentId}">`
+    }
+    if (type == Content.TYPE_NAME_KEY) {
+      // content-header の作成
+      result += Content.getHeader(header);
+      
+      // content-item の作成
+      result += `<div class="${Content.ITEM}">`
+      
+      // content-name の作成
+      result += Content.getItemName(name);
+      
+      // content-keys の作成
+      result += Content.getItemKey(keys);
+      
+      result += `</div>`
+    }
+    if (contentId != null) {
+      result += `</div>`
+    }
     return result;
   }
 }
