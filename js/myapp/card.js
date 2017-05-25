984937311133876336fbaca9b5ec9694383ee46f
Aya Nakazawa <aya@ayatec.top>
2017-05-25 20:06:19 +0900

    Change timing to call view.generateLoading
    in CardController
    from setUser to downloadCard

--- js/myapp/card.js
+++ js/myapp/card.js
@@ -75,13 +75,12 @@ class CardController extends CommonController {
       this.model.LOGIN = false;
     }
     
-    this.view.generateLoading(this.model.$CARD_AREA_SELECTOR, '通信中', `ユーザーID ${this.model.ID} の名刺データを取得中...`);
-    
     this.downloadCard(this.model.ID, this.model.HASH)
   }
   
   downloadCard(_id = null, _hash = null) {
     
+    this.view.generateLoading(this.model.$CARD_AREA_SELECTOR, '通信中', `ユーザーID ${this.model.ID} の名刺データを取得中...`);
   }
 }
 
