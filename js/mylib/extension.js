5e670d64c98567aa6840eefeea61e51c30a3326f
Aya Nakazawa <aya@ayatec.top>
2017-05-27 16:43:09 +0900

    Add String.prototype.capitalize in extension.js

--- js/mylib/extension.js
+++ js/mylib/extension.js
@@ -2,3 +2,7 @@
 Date.prototype.getString = (_format = '%Y/%m/%d %H:%M:%S') => {
   return DatePlus.getDateString(this.date, _format);
 }
+
+String.prototype.capitalize = () => {
+  return this.substring(0, 1).toUpperCase() + this.substring(1);
+}
