0bf78a1e3316b38e26150f430098ed8f87f077aa
Aya Nakazawa <aya@ayatec.top>
2017-05-29 22:05:40 +0900

    Apply Common Upgrade to Card classes

--- js/myapp/card.js
+++ js/myapp/card.js
@@ -6,30 +6,26 @@
 // Model
 
 class CardModel extends CommonModel {
-  constructor({
-    name
-  } = {}) {
-    super({
-      name: name
-    });
-    
-    this.NAME = name;
-    this.EVENT = PS.CE;
+  constructor(
+    _initSetting = {
+      NAME: 'Card Object',
+      VIEW_SPEED_MS: 100,
+      HEADER_TEXT: '名刺情報'
+    }
+  ) {
+    super(_initSetting);
     
     this.CARD_AREA_SELECTOR = '#card-area';
-    this.$CARD_AREA_SELECTOR = $(this.CARD_AREA_SELECTOR);
+    this.CARD_TBODY_SELECTOR = '#card-tbody';
+    
     this.TEMPLATE_CARD_TABLE_SELECTOR = '#card-table-template';
-    this.$TEMPLATE_CARD_TABLE_SELECTOR = $(this.TEMPLATE_CARD_TABLE_SELECTOR);
     this.TEMPLATE_CARD_TBODY_SELECTOR = '#card-tbody-template';
-    this.$TEMPLATE_CARD_TBODY_SELECTOR = $(this.TEMPLATE_CARD_TBODY_SELECTOR);
     this.TEMPLATE_CARD_HOVER_SELECTOR = '#card-hover-template';
-    this.$TEMPLATE_CARD_HOVER_SELECTOR = $(this.TEMPLATE_CARD_HOVER_SELECTOR);
     
-    this.CARD_TBODY = '#card-tbody';
-    this.$CARD_TBODY = $(this.CARD_TBODY);
-    
-    this.VIEW_SPEED_MS = 100;
-    this.HEADER_TEXT = '名刺情報';
+    this.ID = null;
+    this.HASH = null;
+    this.DOWNLOAD = null;
+    this.CARD = null;
     
     this.SELECT = null;
   }
@@ -39,42 +35,47 @@ class CardModel extends CommonModel {
 // View
 
 class CardView extends CommonView {
-  constructor(_model = new CardModel()) {
-    super(_model);
-    
-    this.NAME = 'Card View';
+  constructor(
+    _initSetting = {
+      NAME: 'Card View'
+    }
+  ) {
+    super(_initSetting);
   }
   
-  generateCardArea(_alertType = this.model.ALERT_SUCCESS, _message = null, _close = true) {
-    this.model.$CARD_AREA_SELECTOR.empty();
-    this.model.$CARD_AREA_SELECTOR.append(Content.getHeader(this.model.HEADER_TEXT));
-    this.generateAlert(this.model.$CARD_AREA_SELECTOR, _alertType, _message, _close);
+  generateCardArea(
+    _alertType = this.MODEL.ALERT_SUCCESS,
+    _message = null,
+    _close = true
+  ) {
+    $(this.MODEL.CARD_AREA_SELECTOR).empty();
+    $(this.MODEL.CARD_AREA_SELECTOR).append(Content.getHeader(this.MODEL.HEADER_TEXT));
+    this.generateAlert($(this.MODEL.CARD_AREA_SELECTOR), _alertType, _message, _close);
     
     let template = null;
-    if (this.model.DOWNLOAD) {
+    if (this.MODEL.DOWNLOAD) {
       Log.logClass(this.NAME, 'Card is found');
-      this.model.$CARD_AREA_SELECTOR.append(this.getTemplate(
-        this.model.$TEMPLATE_CARD_TABLE_SELECTOR,
+      $(this.MODEL.CARD_AREA_SELECTOR).append(this.getTemplate(
+        $(this.MODEL.TEMPLATE_CARD_TABLE_SELECTOR),
         {}
       ));
-      this.model.$CARD_TBODY = $(this.model.CARD_TBODY);
       
-      $.each(this.model.CARD, (_id, _val) => {
-        this.model.CARD[_id][this.model.ACTIVE] = false;
-        this.model.$CARD_TBODY.append(this.getTemplate(
-          this.model.$TEMPLATE_CARD_TBODY_SELECTOR,
+      $.each(this.MODEL.CARD, (_id, _val) => {
+        this.MODEL.CARD[_id][this.MODEL.ACTIVE] = false;
+        $(this.MODEL.CARD_TBODY_SELECTOR).append(this.getTemplate(
+          $(this.MODEL.TEMPLATE_CARD_TBODY_SELECTOR),
           {card: _val}
         ));
-        this.model.$CARD_AREA_SELECTOR.append(this.getTemplate(
-          this.model.$TEMPLATE_CARD_HOVER_SELECTOR,
+        $(this.MODEL.CARD_AREA_SELECTOR).append(this.getTemplate(
+          $(this.MODEL.TEMPLATE_CARD_HOVER_SELECTOR),
           {card: _val}
         ));
-        this.model.EVENT.setCardClick(_id);
-        this.model.EVENT.setCardHover(_id);
-        this.model.EVENT.setEditClick(_id);
-        this.model.EVENT.setDeleteClick(_id);
-        this.model.EVENT.setCopyClick(_id);
-        this.model.EVENT.setcloseClick(_id);
+        this.EVENT.setCardClick(_id);
+        this.EVENT.setCardHover(_id);
+        this.EVENT.setEditClick(_id);
+        this.EVENT.setDeleteClick(_id);
+        this.EVENT.setCopyClick(_id);
+        this.EVENT.setcloseClick(_id);
       });
       
     } else {
@@ -82,39 +83,46 @@ class CardView extends CommonView {
     }
   }
   
-  setCardPosition(_id = null) {
+  setCardPosition(
+    _id = null
+  ) {
     if (_id != null) {
       $(`#card-${_id}-detail`).css('top', ($(`#card-${_id}-main`).offset().top + $(`#card-${_id}-main`).height()) + 'px');
     }
   }
   
-  setDetailView(_id = null, _view = null, _type = null, _speed = 0) {
+  setDetailView(
+    _id = null,
+    _view = null,
+    _type = null,
+    _speed = 0
+  ) {
     if (_id != null && _view != null && _type != null) {
       if (_view) {
         this.setCardPosition(_id);
-        if (_type == this.model.HOVER) {
+        if (_type == this.MODEL.HOVER) {
           // ホバーにする
-          this.model.CARD[_id][this.model.HOVER] = true;
-        } else if (_type == this.model.ACTIVE) {
+          this.MODEL.CARD[_id][this.MODEL.HOVER] = true;
+        } else if (_type == this.MODEL.ACTIVE) {
           // アクティブにする
-          this.model.SELECT = _id;
+          this.MODEL.SELECT = _id;
           this.setCardActive(_id, true);
           // ホバー表示済みかで速度を変更
-          if (this.model.CARD[_id][this.model.HOVER]) {
+          if (this.MODEL.CARD[_id][this.MODEL.HOVER]) {
             _speed = 0;
           } else {
-            _speed = this.model.VIEW_SPEED_MS;
+            _speed = this.MODEL.VIEW_SPEED_MS;
           }
         }
         $(`#card-${_id}-detail`).slideDown(_speed);
       } else {
-        if (_type == this.model.HOVER) {
+        if (_type == this.MODEL.HOVER) {
           // ホバーを解除
-          this.model.CARD[_id][this.model.HOVER] = false;
-        } else if (_type == this.model.ACTIVE) {
+          this.MODEL.CARD[_id][this.MODEL.HOVER] = false;
+        } else if (_type == this.MODEL.ACTIVE) {
           // アクティブを解除
-          this.model.SELECT = null;
-          this.model.CARD[_id][this.model.HOVER] = false;
+          this.MODEL.SELECT = null;
+          this.MODEL.CARD[_id][this.MODEL.HOVER] = false;
           this.setCardActive(_id, false);
         }
         $(`#card-${_id}-detail`).slideUp(_speed);
@@ -122,75 +130,18 @@ class CardView extends CommonView {
     }
   }
   
-  setCardActive(_id = null, _flg = null) {
+  setCardActive(
+    _id = null,
+    _flg = null
+  ) {
     if (_id != null && _flg != null) {
       if (_flg) {
-        this.model.CARD[_id][this.model.ACTIVE] = true;
-        $(`#card-${_id}-main`).addClass(this.model.ACTIVE);
+        this.MODEL.CARD[_id][this.MODEL.ACTIVE] = true;
+        $(`#card-${_id}-main`).addClass(this.MODEL.ACTIVE);
       } else {
-        this.model.CARD[_id][this.model.ACTIVE] = false;
-        $(`#card-${_id}-main`).removeClass(this.model.ACTIVE);
+        this.MODEL.CARD[_id][this.MODEL.ACTIVE] = false;
+        $(`#card-${_id}-main`).removeClass(this.MODEL.ACTIVE);
       }
-    }
-  }
-}
-
-// ----------------------------------------------------------------
-// Controller
-
-class CardController extends CommonController {
-  constructor(_obj) {
-    super(_obj);
-    
-    this.model = new CardModel(_obj);
-    this.view = new CardView(this.model);
-    
-    this.NAME = 'Card Controller';
-    this.model.ID = null;
-    this.model.HASH = null;
-    this.model.DOWNLOAD = null;
-    this.model.CARD = null;
-    
-    this.downloadCard();
-  }
-  
-  setUser(_id = null, _hash = null) {
-    this.model.ID = _id;
-    this.model.HASH = _hash;
-    
-    this.downloadCard(this.model.ID, this.model.HASH)
-  }
-  
-  downloadCard(_id = null, _hash = null) {
-    this.model.DOWNLOAD = false;
-    
-    this.view.generateLoading(this.model.$CARD_AREA_SELECTOR, '通信中', `ユーザーID ${_id} の名刺データを取得中`);
-    
-    if (_id != null && _hash != null) {
-      $.ajax({
-        url: 'ruby/getCard.rb',
-        data: {
-          id: _id,
-          password: _hash
-        },
-        dataType: 'json',
-        success: (_data) => {
-          Log.logClassKey(this.NAME, 'ajax getCard', 'success');
-          if (Object.keys(_data).length > 0) {
-            this.model.DOWNLOAD = true;
-            this.model.CARD = _data;
-            this.view.generateCardArea(this.model.ALERT_SUCCESS, `名刺データを取得しました。`);
-          } else {
-            this.view.generateCardArea(this.model.ALERT_INFO, '名刺データは存在しません。', false);
-          }
-        },
-        error: () => {
-          Log.logClassKey(this.NAME, 'ajax getCard', 'failed');
-          this.view.generateCardArea(this.model.ALERT_DANGER, 'ajax通信に失敗しました。', false);
-        }
-      });
-    } else {
-      this.view.generateCardArea(this.model.ALERT_WARNING, 'ログインしてください。', false);
     }
   }
 }
@@ -199,39 +150,31 @@ class CardController extends CommonController {
 // Event
 
 class CardEvent extends CommonEvent {
-  constructor({
-    name = 'Card Event'
-  } = {})
-  {
-    super({
-      name: name
-    });
-    
-    PS.CE = this;
-    
-    this.NAME = name;
-    this.CONTROLLER = new CardController({
-      name: 'Card Controller',
-    });
+  constructor(
+    _initSetting = {
+      NAME: 'Card Event'
+    }
+  ) {
+    super(_initSetting);
   }
   
   setCardClick(_id = null) {
     if (_id != null) {
       $(`#card-${_id}-main`).click(
         () => {
-          if (this.CONTROLLER.model.CARD[_id][this.CONTROLLER.model.ACTIVE]) {
+          if (this.MODEL.CARD[_id][this.MODEL.ACTIVE]) {
             // 既にアクティブなときは解除
-            this.CONTROLLER.view.setDetailView(_id, false, this.CONTROLLER.model.ACTIVE, this.CONTROLLER.model.VIEW_SPEED_MS);
+            this.VIEW.setDetailView(_id, false, this.MODEL.ACTIVE, this.MODEL.VIEW_SPEED_MS);
           } else {
             // アクティブにする
-            $.each(this.CONTROLLER.model.CARD, (_id2, _val2) => {
+            $.each(this.MODEL.CARD, (_id2, _val2) => {
               // 他の項目を解除する
               if (_id2 != _id) {
-                this.CONTROLLER.view.setDetailView(_id2, false, this.CONTROLLER.model.ACTIVE);
+                this.VIEW.setDetailView(_id2, false, this.MODEL.ACTIVE);
               }
             });
             // クリックした項目をアクティブにする
-            this.CONTROLLER.view.setDetailView(_id, true, this.CONTROLLER.model.ACTIVE);
+            this.VIEW.setDetailView(_id, true, this.MODEL.ACTIVE);
           }
         }
       );
@@ -243,16 +186,16 @@ class CardEvent extends CommonEvent {
       $(`#card-${_id}-main`).hover(
         () => {
           // なにもアクティブでないとき
-          if (this.CONTROLLER.model.SELECT == null) {
+          if (this.MODEL.SELECT == null) {
             // ホバーにする
-            this.CONTROLLER.view.setDetailView(_id, true, this.CONTROLLER.model.HOVER, this.CONTROLLER.model.VIEW_SPEED_MS);
+            this.VIEW.setDetailView(_id, true, this.MODEL.HOVER, this.MODEL.VIEW_SPEED_MS);
           }
         },
         () => {
           // アクティブでないとき
-          if (!this.CONTROLLER.model.CARD[_id][this.CONTROLLER.model.ACTIVE]) {
+          if (!this.MODEL.CARD[_id][this.MODEL.ACTIVE]) {
             // ホバー解除
-            this.CONTROLLER.view.setDetailView(_id, false, this.CONTROLLER.model.HOVER);
+            this.VIEW.setDetailView(_id, false, this.MODEL.HOVER);
           }
         }
       );
@@ -294,9 +237,68 @@ class CardEvent extends CommonEvent {
       $(`.card-${_id}-close`).click(
         () => {
           Log.logClassKey(`${this.NAME}:${_id}`, 'Click', 'Close');
-          this.CONTROLLER.view.setDetailView(_id, false, this.CONTROLLER.model.ACTIVE, this.CONTROLLER.model.VIEW_SPEED_MS);
+          this.VIEW.setDetailView(_id, false, this.MODEL.ACTIVE, this.MODEL.VIEW_SPEED_MS);
         }
       );
+    }
+  }
+}
+
+// ----------------------------------------------------------------
+// Controller
+
+class CardController extends CommonController {
+  constructor(
+    _model = {},
+    _initSetting = {
+      NAME: 'Card Controller',
+      MODEL: new CardModel(),
+      VIEW: new CardView(),
+      EVENT: new CardEvent()
+    }
+  ) {
+    super(_model, _initSetting);
+    
+    this.downloadCard();
+  }
+  
+  setUser(_id = null, _hash = null) {
+    this.MODEL.ID = _id;
+    this.MODEL.HASH = _hash;
+    
+    this.downloadCard();
+  }
+  
+  downloadCard(_id = this.MODEL.ID, _hash = this.MODEL.HASH) {
+    this.MODEL.DOWNLOAD = false;
+    
+    this.VIEW.generateLoading($(this.MODEL.CARD_AREA_SELECTOR), '通信中', `ユーザーID ${_id} の名刺データを取得中`);
+    
+    if (_id != null && _hash != null) {
+      $.ajax({
+        url: 'ruby/getCard.rb',
+        data: {
+          id: _id,
+          password: _hash
+        },
+        dataType: 'json',
+        success: (_data) => {
+          Log.logClassKey(this.NAME, 'ajax getCard', 'success');
+          if (Object.keys(_data).length > 0) {
+            this.MODEL.DOWNLOAD = true;
+            this.MODEL.CARD = _data;
+            this.VIEW.generateCardArea(this.MODEL.ALERT_SUCCESS, `名刺データを取得しました。`);
+          } else {
+            this.VIEW.generateCardArea(this.MODEL.ALERT_INFO, '名刺データは存在しません。', false);
+          }
+        },
+        error: () => {
+          Log.logClassKey(this.NAME, 'ajax getCard', 'failed');
+          this.VIEW.generateCardArea(this.MODEL.ALERT_DANGER, 'ajax通信に失敗しました。', false);
+        }
+      });
+    } else {
+      this.VIEW.generateCardArea(this.MODEL.ALERT_WARNING, 'ログインしてください。', false);
     }
   }
 }
