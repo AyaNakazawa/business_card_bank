ceca80887f6eb5a75a759a70e72b443724377602
Aya Nakazawa <aya@ayatec.top>
2017-09-10 21:50:10 +0900

    Update ruby/uploadImage.rb
    Copy the upload image

--- ruby/uploadImage.rb
+++ ruby/uploadImage.rb
@@ -4,3 +4,12 @@ print "Content-type: text/html\n\n";
 require "cgi"
 
 objCgi = CGI.new
+
+file = objCgi["file"]
+dir = "../image/"
+
+fileIo = open(dir + file.original_filename ,"w")
+fileIo.binmode
+fileIo.write(file.read)
+
+puts 'Uploaded: ' + file.original_filename
